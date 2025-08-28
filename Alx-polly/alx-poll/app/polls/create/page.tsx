accc8b115da9114baf5c6b316f0c873e155f6f5c
Vishaylinp <133204106+Vishaylinp@users.noreply.github.com>
2025-08-28 18:16:42 +0200

    Deleted

--- Alx-polly/alx-poll/app/polls/create/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CreatePollPage() {
-  return (
-    <div>
-      <h1>Create New Poll</h1>
-      {/* Poll creation form will go here */}
-    </div>
-  );
-}
--- Alx-polly/alx-poll/app/polls/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PollsPage() {
-  return (
-    <div>
-      <h1>All Polls</h1>
-      {/* Poll list will go here */}
-    </div>
-  );
-}
